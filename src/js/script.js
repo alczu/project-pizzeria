01fa0a2be586eca757b2bffdcc9be91e565a8d63
Aleksander Czulinski <aleksander.czulinski@nfz.gov.pl>
2024-07-29 21:48:39 +0200

    count the product price

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,18 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+
+      thisProduct.renderInMenu();
+    }
+
     initAccordion(){
       const thisProduct = this;
 
